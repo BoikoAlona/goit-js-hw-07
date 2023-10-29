10fd1eca355c5072e06b849991b1dcb1f4b18359
BoikoAlona <a.semikhova@gmail.com>
2023-10-29 19:23:42 +0200

    hw2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(`.gallery`);
+
+function randerList() {
+    const markup = galleryItems
+        .map(({ preview, original, description }) => {
+            return `<li class="gallery__item">
+            <a class="gallery__link" href=${original}>
+            <img class="gallery__image" src =${preview}
+            alt = ${description}/>
+            </a>
+            </li>`;
+        })
+        .join(``);
+    galleryList.innerHTML = markup;
+}
+
+randerList();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
